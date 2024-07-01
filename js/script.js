aa9e642fd182941f1fde6d1cf5278f221b5f3f12
cnadiago <cnadiago@gmail.com>
2024-07-02 00:27:01 +0700

    latihan javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // ini javascript
-prompt()
+function replaceUser(){
+    let User = prompt("Isi nama kamu di sini","Seseorang");
+    document.getElementById("User").innerHTML = User
+    
+}
+
+document.getElementById('tombol').addEventListener("click", function() {replaceUser();})
