3a57711b43377409d09f5ad09ca0ee5dce36c0fb
cnadiago <cnadiago@gmail.com>
2024-07-02 00:50:13 +0700

    latihan javascript 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-// ini javascript
-function replaceUser(){
-    let User = prompt("Isi nama kamu di sini","Seseorang");
-    document.getElementById("User").innerHTML = User
-    
-}
-
-document.getElementById('tombol').addEventListener("click", function() {replaceUser();})
+let NameInput = document.getElementById('Name-Input')
