cdfd5a45f4234a70fdd55b3e514a874e0e0be359
cnadiago <cnadiago@gmail.com>
2024-07-02 22:44:47 +0700

    config web

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let NameInput = document.getElementById('Name-Input');
-document.getElementById('Tombol').addEventListener("click", function () {
-    const Nama = NameInput.value;
-    document.getElementById('User').innerHTML = Nama;
-
-});
-
