cd09e000fa5512bee89d782b26b52a37abef8c06
cnadiago <cnadiago@gmail.com>
2024-07-04 20:26:51 +0700

    Ready sent to RevoU

--- js/prompt.js
+++
@@ -1,7 +0,0 @@
-function replaceUser(){
-    let User = prompt("Isi nama kamu di sini","Seseorang");
-    document.getElementById("User").innerHTML = User;
-
-}
-
-replaceUser();
--- js/script.js
+++ js/script.js
@@ -1,7 +1,137 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let NameInput = document.getElementById('Name');
-    document.getElementById('Submit').addEventListener("click", function () {
-        const Nama = NameInput.value;
-        document.getElementById('User').innerHTML = Nama;
-    });
-});
+function replaceUser(){
+    let User = prompt("Isi nama kamu di sini","Seseorang");
+    document.getElementById("User").innerHTML = User;
+
+}
+
+replaceUser();
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("banner-container");
+    if (n > imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length;
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = 'none';
+    }
+
+    imgList[slideIndex - 1].style.display = 'block';
+}
+
+setInterval(function() {
+    plusDivs(1);
+}, 5000);
+
+
+
+
+var slideIndex1 = 1;
+showDivs1(slideIndex1);
+
+function plusDivs1(n) {
+    showDivs1(slideIndex1 += n);
+}
+
+function showDivs1(n) {
+    var i;
+    var imgList1 = document.getElementsByClassName("Portofolio1");
+    if (n > imgList1.length) slideIndex1 = 1;
+    else if (n < 1) slideIndex1 = imgList1.length;
+
+    for (i = 0; i < imgList1.length; i++) {
+        imgList1[i].style.display = 'none';
+    }
+
+    imgList1[slideIndex1 - 1].style.display = 'block';
+}
+
+
+var slideIndex2 = 1;
+showDivs2(slideIndex2);
+
+function plusDivs2(n) {
+    showDivs2(slideIndex2 += n);
+}
+
+function showDivs2(n) {
+    var i;
+    var imgList2 = document.getElementsByClassName("Portofolio2");
+    if (n > imgList2.length) slideIndex2 = 1;
+    else if (n < 1) slideIndex2 = imgList2.length;
+
+    for (i = 0; i < imgList2.length; i++) {
+        imgList2[i].style.display = 'none';
+    }
+
+    imgList2[slideIndex2 - 1].style.display = 'block';
+}
+
+
+
+var slideIndex3 = 1;
+showDivs3(slideIndex3);
+
+function plusDivs3(n) {
+    showDivs3(slideIndex3 += n);
+}
+
+function showDivs3(n) {
+    var i;
+    var imgList3 = document.getElementsByClassName("Portofolio3");
+    if (n > imgList3.length) slideIndex3 = 1;
+    else if (n < 1) slideIndex3 = imgList3.length;
+
+    for (i = 0; i < imgList3.length; i++) {
+        imgList3[i].style.display = 'none';
+    }
+
+    imgList3[slideIndex3 - 1].style.display = 'block';
+}
+
+
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    var name = document.getElementById('Name').value;
+    var email = document.getElementById('Email').value;
+    var purpose = document.getElementById('Purpose').value;
+    var note = document.getElementById('Note').value;
+
+    var resultHTML = `
+    <p><strong>Nama:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Tujuan:</strong> ${purpose}</p>
+    <p><strong>Pesan:</strong> ${note}</p>
+    `;
+
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = resultHTML;
+
+    resultDiv.classList.remove('no-border');
+    resultDiv.classList.add('with-border');
+     }
+
+
+
+     window.onscroll = function() {scrollFunction()};
+
+            function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                 document.getElementById("myBtn").style.display = "block";
+            } else {
+                 document.getElementById("myBtn").style.display = "none";
+            }
+            }
+
+            function topFunction() {
+                    document.documentElement.scrollTop = 0;
+            }
